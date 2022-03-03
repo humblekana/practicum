7578367a3cef3c6e4bdb2bb6f674a652a75871ba
beta <lnbtkana2326@gmail.com>
2022-03-03 20:57:37 +0700

    save

---
+++ assignment11.2/light-8.c
@@ -0,0 +1,31 @@
+#define F_CPU 16000000
+#include<avr/io.h>
+#include<util/delay.h>
+#include "peri.h"
+
+
+int main(){
+    uint16_t light;
+    init_peri();
+    while(1){
+        light = read_adc(4);
+        if (light < 200){
+            set_led_value(7);
+        }else if (light < 300){
+            set_led_value(6);
+        }else if (light < 400){
+            set_led_value(5);
+        }else if (light < 500){
+            set_led_value(4);
+        }else if (light < 600){
+            set_led_value(3);
+        }else if (light < 700){
+            set_led_value(2);
+        }else if (light < 800){
+            set_led_value(1);
+        }else{
+            set_led_value(0);
+        }
+    }
+    
+}
